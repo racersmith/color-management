2183fae5f472d12ebe29756aecfb93ff9aa433ec
racersmith@gmail.com <racersmith@gmail.com>
2025-03-19 22:07:10 +0000

    Created color_management, _color_test and color_management.png; Edited settings, standard-page.html and theme/parameters.yaml

---
+++ client_code/_color_test/__init__.py
@@ -0,0 +1,85 @@
+from ._anvil_designer import _color_testTemplate
+
+from .. import color_management as cm
+
+
+class _color_test(_color_testTemplate):
+    def __init__(self, **properties):
+        self.init_components(**properties)
+
+        self.button_10.background = cm.DEFAULT_COLOR
+
+        self.button_1.background = cm.get_color("theme:Red")
+
+        self.button_2.background = cm.get_color("theme:MyColor")
+
+        self.button_3.background = cm.get_color("theme:missing")
+
+        self.button_4.background = cm.set_alpha("theme:Red", 0.75)
+        self.button_4.foreground = cm.Color("black").set_alpha(0.25)
+
+        color = "mediumaquamarine"
+        self.button_5.background = cm.get_color(color)
+        rotate = 180 - 30
+        self.button_5.text = f"hue rotate {rotate:+}"
+        self.button_5.foreground = cm.hue_rotate(color, rotate)
+
+        self.button_6.background = cm.Color("lightblue").set_lightness(20)
+        self.button_6.foreground = cm.set_lightness("lightblue", 80)
+
+        self.button_7.background = cm.Color("lightblue").shift_lightness(5)
+        self.button_7.foreground = cm.shift_lightness("lightblue", -50)
+
+        color = "darkcyan"
+        self.button_8.background = (
+            cm.Color(color).shift_lightness(-10).set_alpha(55).hue_rotate(30)
+        )
+        self.button_8.foreground = cm.Color(color)
+
+        self.button_9.background = cm.Color("theme:Circular_A")
+
+    def canvas_1_reset(self, **event_args):
+        """This method is called when the canvas is reset and cleared, such as when the window resizes, or the canvas is added to a form."""
+        c = self.canvas_1
+        c.begin_path()
+        c.move_to(100, 100)
+        c.line_to(100, 200)
+        c.line_to(200, 200)
+        c.close_path()
+
+        triangle_color = cm.Color("#2196F3")
+        c.stroke_style = triangle_color.shift_lightness(-20)
+        c.line_width = 3
+        c.fill_style = triangle_color.set_lightness(80)
+
+        c.fill()
+        c.stroke()
+
+        c.begin_path()
+        c.move_to(80, 120)
+        c.line_to(80, 220)
+        c.line_to(180, 220)
+        c.close_path()
+
+        triangle_color_2 = triangle_color.hue_rotate(120)
+        c.stroke_style = triangle_color_2
+        c.line_width = 3
+        c.fill_style = triangle_color_2.set_alpha(0.5)
+
+        c.fill()
+        c.stroke()
+
+        # Set the stroke and fill styles
+        c.stroke_style = cm.Color("#2196F3").set_lightness(20)
+        c.line_width = 3
+        c.fill_style = cm.Color("#2196F3").set_lightness(80)
+
+        # Draw a filled rectangle
+        c.fill_rect(300, 100, 50, 75)
+
+        c.fill_style = cm.Color("theme:MyColor").set_alpha(0.5)
+        # Draw a filled rectangle
+        c.fill_rect(275, 50, 150, 75)
+
+        # Draw a rectangle outline 25 pixels right of it
+        c.stroke_rect(325, 100, 50, 75)
---
+++ client_code/color_management.py
@@ -0,0 +1,94 @@
+from anvil import app
+
+""" Taking advantage of CSS color functions 
+ref: https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_colors/Relative_colors
+"""
+
+
+DEFAULT_COLOR = "lime"
+
+
+def get_color(color: str, _path=None) -> str:
+    """Get the referenced theme color as necessary
+    This is recursive, so it allows theme colors to reference each other which
+    allows for more descriptive theme color names.  Downside is the color does not resolve
+    in the Color Scheme view.
+
+    Args:
+        color: either a color value ie. hex, rgb, etc. or a referenced theme color in the form 'theme:Color'
+
+    Returns the first resolved color value that is not a theme color reference.
+    """
+
+    if color.startswith("theme:"):
+        if _path is None:
+            _path = set()
+
+        # Handle circular references
+        if color in _path:
+            print(f"Warning: Circular Reference in theme colors {_path}")
+            return DEFAULT_COLOR
+        else:
+            _path.add(color)
+
+        # Get the referenced theme color
+        next_color = app.theme_colors.get(color.lstrip("theme:"), None)
+
+        # Handle if the requested color is not found in the theme
+        if next_color is None:
+            print(
+                f"Warning: Theme color '{color}' not found. Using default color '{DEFAULT_COLOR}'"
+            )
+            return DEFAULT_COLOR
+
+        # Dive to the next level
+        return get_color(next_color, _path=_path)
+    else:
+        # Color is resolved, well done everyone.
+        return color
+
+
+def set_alpha(color: str, alpha: float) -> str:
+    """Set the alpha channel for the color"""
+    color = get_color(color)
+    return f"rgb(from {color} r g b / {alpha})"
+
+
+def hue_rotate(color: str, angle: float) -> str:
+    """Rotate the HSL hue by the given angle"""
+    color = get_color(color)
+    return f"hsl(from {color} calc(h + {angle % 360}) s l)"
+
+
+def set_lightness(color: str, lightness: float) -> str:
+    """Set the LCH lightness channel"""
+    color = get_color(color)
+    return f"lch(from {color} {lightness} c h)"
+
+
+def shift_lightness(color: str, lightness_shift: float) -> str:
+    """Shift the LCH lightness channel"""
+    color = get_color(color)
+    return f"lch(from {color} calc(l + {lightness_shift}) c h)"
+
+
+class Color:
+    def __init__(self, color: str):
+        self.name = color
+        self.color = get_color(color)
+
+    def __str__(self) -> str:
+        """ Get the color string on demand """
+        return self.color
+
+    def set_alpha(self, lightness: float):
+        return Color(set_alpha(self.color, lightness))
+
+    def set_lightness(self, lightness: float):
+        return Color(set_lightness(self.color, lightness))
+
+    def shift_lightness(self, lightness_shift: float):
+        return Color(shift_lightness(self.color, lightness_shift))
+
+    def hue_rotate(self, angle: float):
+        return Color(hue_rotate(self.color, angle))
